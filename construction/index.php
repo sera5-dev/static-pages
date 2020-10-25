6434e93ab3e9e675ac6cdf37bc474ac52d76c0f6
rifqisambas <rifqi@sera5.id>
2020-10-25 09:44:19 +0700

    add construction

---
+++ construction/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>sera5.id</title>
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+</head>
+
+<body>
+	<div class="d-flex justify-content-center align-items-center" style="height: 100vh;">
+		<div class="inline-block align-middle text-center">
+			<div style="margin: 250px;">
+				<h1 class="display-4">Sorry, we're doing <br>some work on the site</h1>
+			</div>
+			<div class="text-center">
+				<a href="https://sera5.id">
+					<img src="https://sera5.id/assets/logo/sera5-dark.png" alt="" srcset="" width="20%">
+				</a>
+			</div>
+		</div>
+	</div>
+</body>
+
+</html>
